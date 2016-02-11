aebebf6fd68857669439ce4d6cbe991158414df4
codedbeardedsignedtaylor <taylorjonessoftware@gmail.com>
2016-02-10 20:29:05 -0600

    2.1.1

---
+++ chapter 2/examples/gear.rb
@@ -0,0 +1,24 @@
+# Object representing a bike gear.
+
+class Gear
+  # attr_reader is an intersting choice because we're not changing the
+  # values in the chainring or cog. They for all purposes are constant.
+  # We can use these constants to create new variables though.
+  attr_reader :chainring, :cog
+
+  def initialize(chainring, cog)
+    @chainring  = chainring
+    @cog        = cog
+  end
+
+  # Calculates the wheel rotations per pedal push on a bike.
+  # It does this by dividing the chainring (slots) by the number
+  # of teeth on a cog.
+
+  def ratio
+    # This call is interesting because it involves converting the
+    # cog to a floar instead of both the chainring and the cog.
+    # This avoids any uncessary conversions to get the job done!
+    chainring / cog.to_f
+  end
+end
